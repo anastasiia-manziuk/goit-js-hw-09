81df4db631407439434df0abb38a20a79b2ff435
anastasiia-manziuk <nastamanzuk62@gmail.com>
2025-09-07 10:25:11 +0000

    Deploying to gh-pages from @ anastasiia-manziuk/goit-js-hw-09@a3de707c6db2aac1dcb300cc282eb19ff67907e9 🚀

--- 2-form.js
+++ 2-form.js
@@ -1,5 +1,4 @@
-import"./assets/modulepreload-polyfill-B5Qt9EMX.js";/* empty css                      */const r=document.querySelector(".container-form"),s=`<form class="feedback-form" autocomplete="off">
-  <label>
+import"./assets/modulepreload-polyfill-B5Qt9EMX.js";/* empty css                      */const t=document.querySelector(".feedback-form"),o=`<label>
     Email
     <input type="email" name="email" autofocus />
   </label>
@@ -7,7 +6,5 @@ import"./assets/modulepreload-polyfill-B5Qt9EMX.js";/* empty css                
     Message
     <textarea name="message" rows="8"></textarea>
   </label>
-  <button type="submit">Submit</button>
-</form>
-`;r.innerHTML=s;const t=document.querySelector(".feedback-form"),l="feedback-form-state",e=JSON.parse(localStorage.getItem(l))||{email:"",message:""};t.elements.email.value=e.email;t.elements.message.value=e.message;t.addEventListener("input",a=>{const{name:o,value:m}=a.target;e[o]=m.trim(),localStorage.setItem(l,JSON.stringify(e))});t.addEventListener("submit",a=>{if(a.preventDefault(),!e.email||!e.message){alert("Fill please all fields");return}console.log(e),localStorage.removeItem(l),t.reset(),e.email="",e.message=""});
+  <button type="submit">Submit</button>`;t.innerHTML=o;const l="feedback-form-state",e=JSON.parse(localStorage.getItem(l))||{email:"",message:""};t.elements.email.value=e.email;t.elements.message.value=e.message;t.addEventListener("input",a=>{const{name:m,value:s}=a.target;e[m]=s.trim(),localStorage.setItem(l,JSON.stringify(e))});t.addEventListener("submit",a=>{if(a.preventDefault(),!e.email||!e.message){alert("Fill please all fields");return}console.log(e),localStorage.removeItem(l),t.reset(),e.email="",e.message=""});
 //# sourceMappingURL=2-form.js.map
